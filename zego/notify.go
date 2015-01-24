cedb253136259e5b83ef4494ac00c30b1e5550b8
wanghaijun <wanghaijun@ztgame.com>
2015-01-23 19:32:37 -0800

    notify interface

--- zego/notify.go
+++ zego/notify.go
@@ -3,7 +3,7 @@ package zego
 func (a Auth) SendEvent(event, msg string) (*Resource, error) {
 
 	path := "apps/notify.json"
-	param := `{"event": "` + event + `", "ap_id": "0", "body":"` + msg + `"}`
+	param := `{"event": "` + event + `", "app_id": "0", "body":"` + msg + `"}`
 	resource, err := api(a, "POST", path, param)
 	if err != nil {
 		return nil, err
